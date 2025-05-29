b63812687f4d59f2065d06cf4afa6776419a4bc5
shahalam26 <149176121+shahalam26@users.noreply.github.com>
2025-05-29 05:29:50 +0000

    variable and data types

---
+++ .devcontainer/01_basicsj/01_variables.js
@@ -0,0 +1,6 @@
+const accountId=14453
+let accountEmail="harmony@gmail.com"
+var accountPassword="12345"
+accountCity ="jaipur"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity]);
---
+++ .devcontainer/01_basicsj/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat all Js code as newer version
+//alert(3+3) // we are using nodejs , not browser
+
+
+//number=> 2 to pwer 53
+//bigint for bigger values
+//string => ""
+//string=>""
+//boolean => true/false
+//null =>standalone value
+//undefined=> value is not defined now
+//symbol 
+//object 
+console.log(typeof undefined);
