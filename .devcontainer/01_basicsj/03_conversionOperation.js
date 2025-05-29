49c5e761543c294c551b0373f6126b38518990f6
shahalam26 <149176121+shahalam26@users.noreply.github.com>
2025-05-29 06:21:50 +0000

    conversions toppic done

---
+++ .devcontainer/01_basicsj/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score="0a"
+console.log(typeof score);
+console.log(typeof(score));
+
+let value=Number(score);
+console.log(typeof value);
+console.log(value)
+
+
+//"2"=> value 2
+//33abc=>NaN
+//true=>1; false=>0
+
+
+let isloggedIn=5
+let booleanisLoggedin=Boolean(isloggedIn);
+console.log(booleanisLoggedin)
+
+// 0 false " " false 
+//any value like 1-any true
+//non empty string true
