e205e16900bb8d1e97e1976af7d5538bdb810dd4
shahalam26 <149176121+shahalam26@users.noreply.github.com>
2025-05-30 06:05:48 +0000

    le date

---
+++ .devcontainer/01_basicsj/06-datesinjs.js
@@ -0,0 +1,9 @@
+//Dates
+let myDate=new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString())
+console.log (myDate.toLocaleString())
+let myCreateDate=new Date  (2023,0,1)
+console.log(myCreateDate.toJSON())
+let myTimeStamp=Date.now()
+console.log(myTimeStamp);
