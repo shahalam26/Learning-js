3db0323ed4d1b9cfd34d8ed2702bba6b61d1248b
shahalam26 <149176121+shahalam26@users.noreply.github.com>
2025-05-30 05:00:45 +0000

    math and num

---
+++ .devcontainer/01_basicsj/05_num_and_maths.js
@@ -0,0 +1,7 @@
+const score=100
+console.log(score)
+const balance = new Number (100)
+console.log(balance)
+console.log(balance.toString().length)
+console.log(balance.toFixed(2));
+console.log(Math.random())
